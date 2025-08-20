0d58ac44a3a81744e41606af97d3d6debe2b2ccb
Sukhbat <1998bsuhe@gmail.com>
2025-08-20 13:49:00 +0900

    added max length on the Customer.birth_date

--- store/models.py
+++ store/models.py
@@ -48,7 +48,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
-    birth_date = models.CharField(null=True)
+    birth_date = models.CharField(max_length=255, null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
